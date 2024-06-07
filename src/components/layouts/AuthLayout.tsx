af7824353e9b9eb4049ea02260f6a19c59854fca
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-06-07 08:04:52 +0800

    update

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -7,13 +7,13 @@ type Props = {
 
 export const AuthLayout: React.FC<Props> = (props) => {
   return (
-    <div className="h-screen w-screen flex flex-col gap-8 justify-center items-center bg-white relative">
+    <div className="h-screen w-screen flex flex-col gap-8 items-center bg-white relative pt-[6%]">
       <img src={BRAND_LOGO} alt="brand-logo.jpeg" className="w-[100px] h-[100px]" />
       <div className="w-[450px]" style={{ zoom: 0.85 }}>
         {props.children}
       </div>
 
-      <p className="text-xs text-gray-200 absolute bottom-3 left-3">App Version v1.0.0.1 (beta)</p>
+      <p className="text-xs text-gray-700 absolute bottom-3 left-3">App Version v1.0.0.1 (beta)</p>
     </div>
   );
 };
